e77905d9b0b85bae0fa901a2c9ff7c48feba7113
Pandit Rathod <panditrathod95@gmail.com>
2022-04-10 23:41:16 +0530

    chage refelcted

--- src/main/java/com/example/rathod/productconsumer/ProductRestConsumer.java
+++ src/main/java/com/example/rathod/productconsumer/ProductRestConsumer.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient("PRODUCT-MICROSERVICE")
+@FeignClient("PRODUCT-MICRO")
 public interface ProductRestConsumer {
 
 	
